dad117c0eeb72357329ab9034fff7bb141cff062
Talvin Ackbaraly <talvin.ackbaraly@icloud.com>
2026-07-20 09:17:58 +0200

    feat: migrate portfolio to Eleventy static site generator with localized data support

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("cv");
+  eleventyConfig.addPassthroughCopy("js");
+
+  eleventyConfig.addPassthroughCopy("robots.txt");
+  eleventyConfig.addPassthroughCopy("sitemap.xml");
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+  };
+};
